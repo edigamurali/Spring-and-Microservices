07fbfd3d9f09081b6d16ac7e3e30423bf3f882ec
edigamurali <edigamurali35@gmail.com>
2025-01-13 08:14:01 +0530

    Add files via upload

---
+++ BootMVCProject01-FirstApp/src/main/java/com/nt/BootMvcProject01FirstAppApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcProject01FirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcProject01FirstAppApplication.class, args);
+	}
+
+}
